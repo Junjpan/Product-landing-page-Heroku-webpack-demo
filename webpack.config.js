d115e8e10829d8bfd8c91ee02ed63820f1e83c73
Jun Pan <merrypjj@yahoo.com>
2019-09-12 14:48:57 -0700

    change port

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path=require('path');
-const HtmlWebpackPlugin=require('html-webpack-plugin')
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const server_port=process.env.PORT||5000;
 
 module.exports={
  entry:'./src/index.js',
@@ -7,6 +8,11 @@ module.exports={
      filename:"main.js",
      path:path.resolve(__dirname,"dist")
  },
+ devServer:{
+    contentBase:path.join(__dirname,'dist'),
+    compress:true,
+    port:server_port
+},
  plugins:[new HtmlWebpackPlugin({template:'./src/template.html'})],
  module:{
      rules:[{
