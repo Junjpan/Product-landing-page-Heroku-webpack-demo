4ec1cc73a3d364c1946f45d116906ed8c74b49b5
Jun Pan <merrypjj@yahoo.com>
2019-09-12 16:40:47 -0700

    add port and host

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path=require('path');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
-
+var server_port = process.env.PORT || 5000;
+var server_host = '0.0.0.0';
 
 
 module.exports={
@@ -9,6 +10,14 @@ module.exports={
      filename:"main.js",
      path:path.resolve(__dirname,"dist")
  },
+ devServer: {
+    disableHostCheck: true,
+    contentBase: './dist',
+    compress: true,
+    inline: true,
+    port:server_port,
+    host:server_host 
+   },
  plugins:[new HtmlWebpackPlugin({template:'./src/template.html'})],
  module:{
      rules:[{
