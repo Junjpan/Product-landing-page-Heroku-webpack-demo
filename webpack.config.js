cb8435c0dda46bca1ada04b09ed41caafd9b4473
Jun Pan <merrypjj@yahoo.com>
2019-09-12 14:23:09 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path=require('path');
+const HtmlWebpackPlugin=require('html-webpack-plugin')
+
+module.exports={
+ entry:'./src/index.js',
+ output:{
+     filename:"main.js",
+     path:path.resolve(__dirname,"dist")
+ },
+ plugins:[new HtmlWebpackPlugin({template:'./src/template.html'})],
+ module:{
+     rules:[{
+         test:/\.css$/,
+         use:['style-loader',
+              'css-loader']
+     },
+     {test:/\.html$/,
+      use:['html-loader'] },
+     {test:/\.(svg|jpg|gif|png)$/,
+      use:{
+          loader:"file-loader",
+          options:{name:"[name].[hash].[ext]",
+                   outputPath:"Images"}
+      }}]
+ }
+
+}
