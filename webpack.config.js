da31c107fef06e736d538a1c7a35920c792b752a
Jun Pan <merrypjj@yahoo.com>
2019-09-12 16:36:52 -0700

    remove port and host-test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path=require('path');
 const HtmlWebpackPlugin=require('html-webpack-plugin');
-var server_port = process.env.PORT || 5000;
-var server_host = '0.0.0.0';
+
 
 
 module.exports={
@@ -11,14 +10,6 @@ module.exports={
      path:path.resolve(__dirname,"dist")
  },
  plugins:[new HtmlWebpackPlugin({template:'./src/template.html'})],
- devServer: {
-    disableHostCheck: true,
-    contentBase: './dist',
-    compress: true,
-    inline: true,
-    port:server_port,
-    host:server_host 
-   },
  module:{
      rules:[{
          test:/\.css$/,
